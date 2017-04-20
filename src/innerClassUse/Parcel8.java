023e0fd40132e6af3c4e5698ecf5899c38ac0fd7
kang <k1063178819@gmail.com>
2017-04-20 14:17:51 +0800

    内部类

--- src/innerClassUse/Parcel8.java
+++ src/innerClassUse/Parcel8.java
@@ -14,10 +14,22 @@ public class Parcel8 {
             }
         };
     }
+    //匿名内部类中使用外部类定义的对象，要加上final修饰符
+    public StringDefine stringDefine( String name){
+        return new StringDefine(){
+          private String string = name;
+
+
+        };
+    }
 
     public static void main(String[] args) {
         Parcel8 parcel8=new Parcel8();
         Wrapping wrapping =parcel8.wrapping(1);
         wrapping.value();
+
+        StringDefine stringDefine = parcel8.stringDefine("hello");
+
+
     }
 }
---
+++ src/innerClassUse/StringDefine.java
@@ -0,0 +1,10 @@
+package innerClassUse;
+
+/**
+ * Created by kang on 17/4/18.
+ */
+public class StringDefine {
+
+
+
+}
